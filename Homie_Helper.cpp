4daf22c6f292021b47a9db87fcf9dc62a0d08b14
au <alexeyu@aspentechlabs.com>
2017-11-01 21:58:53 +0200

    initial

--- Homie_Helper.cpp
+++ Homie_Helper.cpp
@@ -1,23 +1,24 @@
+#include <Homie.h>
 #include <Homie_Helper.h>
 #include <ArduinoOTA.h>
 
-void ota_setup(char* password, byte led) {
+void ota_setup(char* password) {
   ArduinoOTA.setPassword(password);
   ArduinoOTA.onStart([]() {});
   ArduinoOTA.onEnd([]() {
-    digitalWrite(led, LOW);
+    digitalWrite(D0, LOW);
     for (int i = 0; i < 20; i++)
        {
-         digitalWrite(led, HIGH);
+         digitalWrite(D0, HIGH);
          delay(i * 2);
-         digitalWrite(led, LOW);
+         digitalWrite(D0, LOW);
          delay(i * 2);
        }
-       digitalWrite(led, HIGH);
+       digitalWrite(D0, HIGH);
        ESP.restart();
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    digitalWrite(led, total % 1);
+    digitalWrite(D0, total % 1);
   });
   ArduinoOTA.onError([](ota_error_t error) {
     if (error == OTA_AUTH_ERROR);          // Auth failed
@@ -27,4 +28,18 @@ void ota_setup(char* password, byte led) {
     else if (error == OTA_END_ERROR);      // End failed
   });
   ArduinoOTA.begin();
+}
+
+void ota_handle() {
+  ArduinoOTA.handle();
+}
+void readSend(HomieNode* n, Data* d, Setting* s, double (*f)()){
+    if (millis() - d->lastRead >= s->intervalRead) {
+        double v = (*f)();
+        if (!isnan(v) && v != d->vLast && millis() - d->lastSent >= s->intervalSend) {
+          d->vLast = v;
+          n->setProperty("value").send(String(v));
+          d->lastSent = millis();
+        }
+    }
 }
--- Homie_Helper.h
+++ Homie_Helper.h
@@ -1,6 +1,22 @@
 #ifndef Homie_Helper_H
 #define Homie_Helper_H
 
-void ota_setup(char* password, byte led);
+typedef struct {
+    byte pin;
+    int intervalRead;
+    int intervalSend;
+    int eeprom;
+} Setting;
+typedef struct {
+    double vLast;
+    double vCurr;
+    unsigned long lastRead;
+    unsigned long lastSent;
+} Data;
+
+void ota_setup(char* password);
+void ota_handle();
+
+void readSend(HomieNode* n, Data* d, Setting* s, double (*f)());
 
 #endif
