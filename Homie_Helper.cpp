5e90fe699167318cd50be4f87245c9f6846dccb0
au <alexeyu@aspentechlabs.com>
2018-12-18 00:08:56 +0200

    a

--- Homie_Helper.cpp
+++ Homie_Helper.cpp
@@ -33,6 +33,11 @@ void onHomieEvent(const HomieEvent& event) {
 }
 
 void pre_setup() {
+  lastWifiConnected = 0;
+  lastWifiDisconnected = millis();
+  lastMqttConnected = 0;
+  lastMqttDisconnected = millis();
+  restartInitiated = false;
   Homie.onEvent(onHomieEvent);
 }
 
